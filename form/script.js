47f22782ec03307616f03bb4730201db7c676289
Shivadevil797 <shiva.g.trader797@gmail.com>
2025-05-31 00:44:26 +0530

    Update script.js

--- form/script.js
+++ form/script.js
@@ -22,7 +22,7 @@ const db = getFirestore(app);
 window.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("loginForm");
 
-  loginForm.addEventListener("submitBtn", (e) => {
+  loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
