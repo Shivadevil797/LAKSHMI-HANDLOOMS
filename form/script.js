d15d01aba5d8f395e75146d79b9bf29627741c75
Shivadevil797 <shiva.g.trader797@gmail.com>
2025-05-31 00:59:49 +0530

    up

--- form/script.js
+++ form/script.js
@@ -20,15 +20,14 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 window.addEventListener("DOMContentLoaded", () => {
-  const loginForm = document.getElementById("loginForm");
-
-  loginForm.addEventListener("submit", (e) => {
+    const loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+    .then((userCredential) => {
         const user = userCredential.user;
         console.log("User signed in:", user);
         alert("Login successful!");
